ff77c8adc3df5ed687a09686b5e398b21ed72b67
David Battefeld <dbt@wegewerk.com>
2023-05-24 16:43:56 +0200

    feat: add module

--- src/window.py
+++ src/window.py
@@ -15,18 +15,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk
+from gi.repository import Gtk, Adw, Gio
 
+import sys
+#sys.path.append("./classes")
+from .API import API
 
 @Gtk.Template(resource_path='/de/davidbattefeld/envoye/window.ui')
 class EnvoyeWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'EnvoyeWindow'
 
     main_box = Gtk.Template.Child()
+    api_test_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        api_test_action = Gio.SimpleAction(name="api_test")
+        api_test_action.connect("activate", self.test_action)
+        self.add_action(api_test_action)
+
+    def test_action(self, action, _):
+      api = API()
+      api.loadAndPrintLabels()
+      #print(sys.path)
+      #print("button pressed!")
 
 class AboutDialog(Gtk.AboutDialog):
 
